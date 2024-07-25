75944ca5baa5bc7fd8c3fbc2eae86e29c3b5949f
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-07-25 12:42:29 +0530

    Lab011

---
+++ Lab011.java
@@ -0,0 +1,11 @@
+package ex_July;
+
+public class Lab011 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to the Java training");
+
+        System.out.print("8+8");
+        System.out.print("5*5");
+    }
+}
