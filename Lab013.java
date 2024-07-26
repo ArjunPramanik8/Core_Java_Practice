557b5b16ab8e389aca5be793645d4825ba4f2955
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-07-26 12:15:32 +0530

    Lab013

---
+++ Lab013.java
@@ -0,0 +1,12 @@
+package July;
+
+public class Lab013 {
+    // Can I run class without a main method?
+    // run option?
+    static public void  main(String[] args) {
+
+    }
+    public static void main() {
+
+    }
+}
