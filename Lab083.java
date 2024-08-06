96c099f069a41a2cfa4f3139e60d06bea17284a1
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-08-06 10:53:55 +0530

    Lab083

---
+++ Lab083.java
@@ -0,0 +1,13 @@
+package August
+
+public class Lab083 {
+    public static void main(String[] args) {
+        int a  = 10;
+        // a = 4 -> TRUE OR FALSE
+        if( a == 4){
+            System.out.println("Haha");
+        }else{
+            System.out.println("LOL");
+        }
+    }
+}
