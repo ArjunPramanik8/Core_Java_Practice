c3a7bc2e0463175e25d5a5a72f285164f56a8a23
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-08-06 10:54:15 +0530

    Lab084

---
+++ Lab084.java
@@ -0,0 +1,13 @@
+package August
+
+public class Lab084 {
+    public static void main(String[] args) {
+        boolean b = !true;
+        if(b){
+            System.out.println("b is true");
+        }else {
+            System.out.println("b is not true");
+        }
+
+    }
+}
