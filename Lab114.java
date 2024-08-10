4d3a6b72ada9c4f43f64d93cf3f10e7c520dcb40
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-08-10 11:10:36 +0530

    Lab114

---
+++ Lab114.java
@@ -0,0 +1,10 @@
+package August
+
+public class Lab114 {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'z';c++){
+            System.out.println(c);
+        }
+
+    }
+}
