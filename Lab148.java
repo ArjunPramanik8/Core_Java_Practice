b089eabf4fcb91b09db0b8dbd8048d5d136557ca
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-09-10 23:07:57 +0530

    Lab148

--- Lab148.java
+++ Lab148.java
@@ -1,8 +1,13 @@
-package july.ex_13072024;
+package August
 
-public class Lab032 {
+public class Lab016 {
     public static void main(String[] args) {
-        final double PIE = 3.14;
-        //PIE = 3.145;
+        byte b  = 10;
+        System.out.println(b);
+        // JVM -> brother who will execute the main method
+        // 10 - Decimal
+        // Machine(0,1) -> 00001010 - 8 bits = 1 Byte
+        long distance_moon_earth = 3844000000000000l;
+
     }
 }
