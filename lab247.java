c52996096f9ae82533a515e5bb704ab56848b1ef
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-09-16 23:03:58 +0530

    Practice

--- lab247.java
+++ lab247.java
@@ -1,26 +1,21 @@
 package july.ex_20072024;
 
-import java.util.Scanner;
-
-public class Lab101 {
+public class Lab102 {
     public static void main(String[] args) {
-        // Write a program name, age, salary - print it
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name");
-        String name = sc.next();
 
-        System.out.println("Enter your age");
-        int age = sc.nextInt();
+        int a = 11;
+        switch (-1){
+            default:
+                System.out.println("Default");
+                break;
+            case -1:
+                System.out.println("10");
+                break;
+            case 9:
+                System.out.println("10");
+                break;
+        }
 
-        System.out.println("Enter your Salary");
-        double salary = sc.nextDouble();
-
-        System.out.println("Your details are " + name);
-        System.out.println("Your age is " + age);
-        System.out.println("Your Salary is " + salary);
-
-
-        sc.close();
 
     }
 }
