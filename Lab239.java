0bbdb8ebde315d9c8eb1005e5cb84ffbd0357799
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-09-17 23:46:47 +0530

    Practice

--- Lab239.java
+++ Lab239.java
@@ -1,31 +1,17 @@
 package july.ex_20072024;
 
-import java.util.Scanner;
-
-public class Lab104 {
+public class Lab100 {
     public static void main(String[] args) {
-        //Program to Find the Largest Among Three Numbers:
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Num 1");
-        int num1 = sc.nextInt();
-        System.out.println("Enter the Num 2");
-        int num2 = sc.nextInt();
-        System.out.println("Enter the Num 3");
-        int num3 = sc.nextInt();
-
-
-        // num 1 > num2 && num1 > num3 ->  num1
-        // num 2 > num1 && num2 > num3 ->  num2
-        // num 3
-
-        if (num1 >= num2 && num1 >= num3) {
-            System.out.println(num1);
-        } else if (num2 >= num1 && num2 >= num3) {
-            System.out.println(num2);
-        } else {
-            System.out.println(num3);
-        }
-
-
+        // JDK > 13
+        char code = 'B';
+        int val = switch (code){
+            case 'A':
+                yield 65;
+            case 'B':
+                yield 66;//// return
+            default:
+                throw new IllegalStateException("Unexpected value: " + code);
+        };
+        System.out.println(val);
     }
 }
