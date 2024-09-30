7eb84e9163cbb84e70e4a6c2b10680e3f25c0a47
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-09-30 22:40:31 +0530

    Update Lab004.java

--- Lab004.java
+++ Lab004.java
@@ -1,7 +1,9 @@
 package July;
 
 public class Lab004 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        System.out.println("Mathematical Calculation");
 
         // Package -> Class -> Main Method -> statements(instructions)
         System.out.println(2+2);
