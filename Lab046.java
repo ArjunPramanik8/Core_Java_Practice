3a2ea2f3a3ddd6b4b2c48991f534e0f9b091ff50
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-02 23:52:12 +0530

    Update Lab046.java

--- Lab046.java
+++ Lab046.java
@@ -1,7 +1,8 @@
 package July
 
 public class Lab046 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         boolean a = true;
         System.out.println(!a);
         System.out.println(!(10>20));
