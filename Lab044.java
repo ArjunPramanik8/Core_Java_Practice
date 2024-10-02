4ab68bd13ed3c99aabfacc6bebf7a7105ac2e295
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-02 23:51:34 +0530

    Update Lab044.java

--- Lab044.java
+++ Lab044.java
@@ -1,7 +1,8 @@
 package July
 
 public class Lab044 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int a = 10;
         int b = 10;
         boolean c = (10 >= 10); // 10 > 10 or 10 = 10
