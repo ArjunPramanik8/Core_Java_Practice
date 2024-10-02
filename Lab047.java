c2ac82647720aef5c665936fd59f78178325d1a7
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-02 23:53:05 +0530

    Update Lab047.java

--- Lab047.java
+++ Lab047.java
@@ -3,7 +3,6 @@ package July
 public class Lab047 {
     public static void main(String[] args) {
         // || - OR
-
         System.out.println(true || true);
         System.out.println(true || false);
         System.out.println(false || true);
