43240226e6feb5dc409b6f77ecebb31740518ff2
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-03 23:47:47 +0530

    Update and rename Lab002.java to MultiplyTwoNumbers.java

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,7 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        int a = 3, b = 7;
+        int product = a * b;
+        System.out.println("Product: " + product);
+    }
+}
