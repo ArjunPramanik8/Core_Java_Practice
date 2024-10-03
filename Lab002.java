f088b088dd2b2a252ecb08cabefb62eed453ff10
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-03 22:44:41 +0530

    Update Lab002.java

--- Lab002.java
+++ Lab002.java
@@ -1,7 +1,11 @@
 package July;
 
 public class Lab002 {
-    public static void main(String[] args) {
-        System.out.println("Hello World Arjun!");
+    public static void main(String[] args)
+     { 
+        // Addition of two numbers
+        int a = 5, b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
