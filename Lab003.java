fd1c543dd9f1d8ebfc81996cf2076e2a6e404970
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-03 23:45:38 +0530

    Update Lab003.java

--- Lab003.java
+++ Lab003.java
@@ -3,24 +3,12 @@ package July;
 public class Lab003 {
 
 
-    public static void main(String[] args) {
-        // Single Comment - This code will not run or executed
-        //  2+2 ?
-
-        // Purpose Comment ?
-        // Instructions to Jr QA or Dev
-        // explain
-
-        /**
-         * Multiple Comment
-         * Documentation Purpose
-         *
-         *
-         *
-         */
-
-
-
+    public static void main(String[] args) 
+{
+ //MultiplyTwoNumbers
+        int a = 3, b = 7;
+        int product = a * b;
+        System.out.println("Product: " + product);
 
     }
 
