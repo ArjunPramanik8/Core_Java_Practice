7fa5e879ca87ed111514b3e97f52a45e27e35c48
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-05 23:43:08 +0530

    Update SwapNumbers.java

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,5 +1,6 @@
 public class SwapNumbers {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+   {
         int a = 5, b = 10;
         int temp = a;
         a = b;
