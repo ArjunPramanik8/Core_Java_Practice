04a031ef657aeffb4b02517af1d4fa022057cdab
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-05 23:42:19 +0530

    Update MultiplyTwoNumbers.java

--- MultiplyTwoNumbers.java
+++ MultiplyTwoNumbers.java
@@ -1,8 +1,8 @@
 public class MultiplyTwoNumbers
  {
     public static void main(String[] args) 
-{       int a = 3, b = 7;
+   {       int a = 3, b = 7;
         int product = a * b;
-        System.out.println("Product: " + product);
-    }
-}
+        System.out.println("Product: " + product); 
+   }
+  }
