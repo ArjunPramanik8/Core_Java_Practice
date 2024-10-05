1b8450c30a58a9c869095a2811236b9cf5850deb
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-05 23:40:43 +0530

    Update LargestOfTwo.java

--- LargestOfTwo.java
+++ LargestOfTwo.java
@@ -1,5 +1,6 @@
-public class LargestOfTwo {
-    public static void main(String[] args) {
+public class LargestOfTwo
+ {
+     public static void main(String[] args) {
         int a = 10, b = 20;
         int largest = a > b ? a : b;
         System.out.println("Largest: " + largest);
