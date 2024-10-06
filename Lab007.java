330dd08263603661f345648d938cb5ab10e2aeda
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-06 23:25:00 +0530

    Update Lab007.java

--- Lab007.java
+++ Lab007.java
@@ -1,31 +1,9 @@
-package ex_July;
-
-public class Lab007 {
-    public static void main(String[] pramod) {
-        
-        //Program to Find the Largest Among Three Numbers:
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Num 1");
-        int num1 = sc.nextInt();
-        System.out.println("Enter the Num 2");
-        int num2 = sc.nextInt();
-        System.out.println("Enter the Num 3");
-        int num3 = sc.nextInt();
-
-
-        // num 1 > num2 && num1 > num3 ->  num1
-        // num 2 > num1 && num2 > num3 ->  num2
-        // num 3
-
-        if (num1 >= num2 && num1 >= num3) {
-            System.out.println(num1);
-        } else if (num2 >= num1 && num2 >= num3) {
-            System.out.println(num2);
-        } else {
-            System.out.println(num3);
+public class SumOfFirstTen {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
         }
-
-
-    }
+        System.out.println("Sum: " + sum);
     }
 }
