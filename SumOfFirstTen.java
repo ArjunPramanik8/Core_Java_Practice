0385afdc3fb4854aac6d63e6fbb21ca3659edfb4
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-06 23:25:29 +0530

    Update SumOfFirstTen.java

--- SumOfFirstTen.java
+++ SumOfFirstTen.java
@@ -1,5 +1,6 @@
 public class SumOfFirstTen {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+ {
         int sum = 0;
         for (int i = 1; i <= 10; i++) {
             sum += i;
