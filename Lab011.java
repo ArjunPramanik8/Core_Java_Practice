62a9e97d2b79fa46bc487916c15863d6c4684ecf
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-09 20:58:22 +0530

    Update Lab011.java

--- Lab011.java
+++ Lab011.java
@@ -1,11 +1,7 @@
-package ex_July;
-
-public class Lab011 {
+public class ReverseString {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Welcome to the Java training");
-
-        System.out.print("8+8");
-        System.out.print("5*5");
+        String str = "hello";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(reversed);
     }
 }
