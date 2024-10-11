4db41ddf06de86773139ebbfa3413c090d8f65b6
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-11 17:40:22 +0530

    Update Lab016.java

--- Lab016.java
+++ Lab016.java
@@ -1,13 +1,10 @@
-package July
-
-public class Lab016 {
+public class SumOfDigits {
     public static void main(String[] args) {
-        byte b  = 10;
-        System.out.println(b);
-        // JVM -> brother who will execute the main method
-        // 10 - Decimal
-        // Machine(0,1) -> 00001010 - 8 bits = 1 Byte
-        long distance_moon_earth = 3844000000000000l;
-
+        int num = 1234, sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("Sum: " + sum);
     }
 }
