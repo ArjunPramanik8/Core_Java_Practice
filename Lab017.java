bbf674089ddf2f24c0cb68f0cd31e0cfa81834d0
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-16 23:41:55 +0530

    Update Lab017.java

--- Lab017.java
+++ Lab017.java
@@ -1,16 +1,10 @@
-package July
-
-public class Lab017 {
+public class SumOfDigits {
     public static void main(String[] args) {
-        // age?
-
-        //max age of theperson in world ? -
-        byte age= 122; // age > 127 // less memory -> smart container - good practice
-        int age1 = 100; // wasting memory - 214768348488
-
-        // program store pi with only 2 decimal 3.14, 3.142323237866999
-        float pi = 3.14f;
-        double pi_d =  3.14876545678;
-
+        int num = 1234, sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("Sum: " + sum);
     }
 }
