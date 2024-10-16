8ee930dabe3a5bdfaf718c39f00cdc778624719a
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-16 23:46:23 +0530

    Update Lab018.java

--- Lab018.java
+++ Lab018.java
@@ -1,24 +1,7 @@
-package July
-
-public class Lab018 {
+public class Palindrome {
     public static void main(String[] args) {
-
-        //byte b = 128; //-127 to 127
-
-        char c = 'A';
-        System.out.println(c);
-
-        char c1 = '$';
-        System.out.println(c1);
-
-        char c2 = '\n';
-        System.out.println(c2);
-
-        char c3 = '\u1F6A';  //:)
-        System.out.println(c3);
-        // :), :(, :D .;) -> unicode chars
-
-
-
+        String str = "madam";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(str.equals(reversed) ? "Palindrome" : "Not Palindrome");
     }
 }
