a0e922a953c2a7d3303ec2b17906ebdb36ba03ca
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-16 23:44:49 +0530

    Update Lab017.java

--- Lab017.java
+++ Lab017.java
@@ -1,6 +1,7 @@
 public class SumOfDigits
  {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+       {
         int num = 1234, sum = 0;
         while (num > 0) {
             sum += num % 10;
