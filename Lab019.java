38701cfee2f9ee91f8e1089cfd41ed3699f2a7aa
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-17 22:54:20 +0530

    Update Lab019.java

--- Lab019.java
+++ Lab019.java
@@ -1,24 +1,8 @@
-package July
-
-public class Lab020 {
+import java.util.Arrays;
+public class SecondLargest {
     public static void main(String[] args) {
-      
-
-        byte b = 127;
-        //b = b+1;
-        System.out.println(b);
-
-        char grade = 'A';
-        char grade_kusum = 'E';
-        char grade_wswati = 'B';
-        char c = '!';
-        char c2 = '@';
-        char _123 = 'N';
-
-        // Can I change the data type of variable between program
-        // No
-        int age = 123;
-
-
+        int[] arr = {10, 20, 5, 30, 15};
+        Arrays.sort(arr);
+        System.out.println("Second Largest: " + arr[arr.length - 2]);
     }
 }
