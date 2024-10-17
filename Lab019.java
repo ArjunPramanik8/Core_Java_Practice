13694bab214122e8763ad486d4e8d5df0bce7c54
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-17 22:54:45 +0530

    Update Lab019.java

--- Lab019.java
+++ Lab019.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 
-public class SecondLargest {
-    public static void main(String[] args) {
+public class SecondLargest
+ {
+    public static void main(String[] args) 
+    {
         int[] arr = {10, 20, 5, 30, 15};
         Arrays.sort(arr);
         System.out.println("Second Largest: " + arr[arr.length - 2]);
