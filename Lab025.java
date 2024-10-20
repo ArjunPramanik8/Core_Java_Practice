01ddeef8baa75874579a5486e1a51c7d1f390941
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-21 00:58:26 +0530

    Update Lab025.java

--- Lab025.java
+++ Lab025.java
@@ -1,28 +1,14 @@
-package July
-
-public class Lab025 {
+public class CountCharacter {
     public static void main(String[] args) {
-//        int a = 10;
-//        int b= 34;
-//        System.out.println(a+b);
-//
-//        String s1 = "PRamod";
-//        String s2 = "Dutta";
-//        System.out.println(s1+s2);
+        String str = "hello world";
+        char target = 'o';
+        int count = 0;
 
-        String s3 = "Amit";
-        int x = 99;
-        int y = 100;
-
-//        System.out.println(x+y+s3);
-//        System.out.println(s3+x+y);
-//        System.out.println(x+s3+y);
-
-
-        System.out.println(x+y+s3+y+s3+x+y);
-
-
-
-
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == target) {
+                count++;
+            }
+        }
+        System.out.println("Occurrences of '" + target + "': " + count);
     }
 }
