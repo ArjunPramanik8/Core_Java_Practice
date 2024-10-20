d8e74348a19c1e015a8bcdba54cb088e57e27a1b
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-20 14:40:30 +0530

    Update Lab022.java

--- Lab022.java
+++ Lab022.java
@@ -3,7 +3,8 @@ public class GCD
     public static void main(String[] args) 
   {
         int a = 36, b = 60;
-        while (a != b) {
+        while (a != b) 
+            {
             if (a > b) {
                 a = a - b;
             } else {
