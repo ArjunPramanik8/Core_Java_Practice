c2a83b0928db82cc57a5d3a1617b9659bacb3557
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-23 23:37:49 +0530

    Update Lab026.java

--- Lab026.java
+++ Lab026.java
@@ -1,8 +1,15 @@
-package July
-
-public class Lab026 {
+public class LCM {
     public static void main(String[] args) {
-        String name = "Pramod";
-        System.out.printf("Your name is -> %s",name);
+        int a = 15, b = 20, lcm;
+
+        lcm = (a > b) ? a : b;
+
+        while (true) {
+            if (lcm % a == 0 && lcm % b == 0) {
+                System.out.println("LCM of " + a + " and " + b + " is " + lcm);
+                break;
+            }
+            lcm++;
+        }
     }
 }
