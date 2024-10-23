ebd41900ae64f972fdb7e3fb32331c0a1fd66973
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-23 23:41:47 +0530

    Update Lab027.java

--- Lab027.java
+++ Lab027.java
@@ -1,5 +1,6 @@
 public class TransposeMatrix {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+         {
         int[][] matrix = {
             {1, 2, 3},
             {4, 5, 6},
