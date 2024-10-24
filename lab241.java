e0ca4d21b35cf7eeb268c49e8dbe41c21f55d140
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-24 16:03:53 +0530

    Update lab241.java

--- lab241.java
+++ lab241.java
@@ -1,14 +1,21 @@
-package july.ex_20072024;
-
-public class Lab105 {
+public class BubbleSort {
     public static void main(String[] args) {
-
-        char ch = 'a';
-        if ( ch == 'a' || ch == 'e' || ch =='o' || ch == 'i' || ch == 'u'){
-            System.out.println("Vowel");
-        }else{
-            System.out.println("Consonant");
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
 
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
