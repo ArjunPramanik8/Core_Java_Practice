b34f5940bd02b217e33864998ca15319343a17ec
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-28 01:19:10 +0530

    Update Lab032.java

--- Lab032.java
+++ Lab032.java
@@ -1,8 +1,15 @@
-package July
+import java.util.Arrays;
 
-public class Lab032 {
+public class BinarySearch {
     public static void main(String[] args) {
-        final double PIE = 3.14;
-        //PIE = 3.145;
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+        int result = Arrays.binarySearch(arr, key);
+
+        if (result >= 0) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
     }
 }
