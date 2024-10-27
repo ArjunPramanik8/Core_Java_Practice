42fae4320d8f8af74b143c734fdd4354b1d7f4d9
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-10-27 23:52:02 +0530

    Update Lab030.java

--- Lab030.java
+++ Lab030.java
@@ -1,9 +1,23 @@
-package July
+import java.util.Arrays;
 
-public class Lab030 {
+public class AnagramCheck {
     public static void main(String[] args) {
-        int age = 65;
-        //double age = 45;
-        //. Local - within method
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (str1.length() == str2.length()) {
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            if (Arrays.equals(arr1, arr2)) {
+                System.out.println("The strings are anagrams.");
+            } else {
+                System.out.println("The strings are not anagrams.");
+            }
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
     }
 }
