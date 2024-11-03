d17d95b3fb01bfd217d2dcdf170eceeedcdc3119
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-03 22:30:52 +0530

    Update Lab034.java

--- Lab034.java
+++ Lab034.java
@@ -4,7 +4,8 @@ public class SelectionSort {
         int[] arr = {29, 10, 14, 37, 13};
         int n = arr.length;
 
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n - 1; i++) 
+                {
             int minIdx = i;
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIdx]) {
