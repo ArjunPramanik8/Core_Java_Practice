bcd95736401546ca01b9f1d93b8a5fbf029f4758
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-03 22:30:29 +0530

    Update Lab034.java

--- Lab034.java
+++ Lab034.java
@@ -1,9 +1,23 @@
-package July
-
-public class Lab034 {
+public class SelectionSort {
     public static void main(String[] args) {
-        //int enum = 45;
-        int Enum = 45;
-        System.out.println(Enum);
+        int[] arr = {29, 10, 14, 37, 13};
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
