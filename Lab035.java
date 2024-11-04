c1fe3c8cafd2abd9412d4abc41fc5bac5ac25c7e
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-04 22:58:04 +0530

    Update Lab035.java

--- Lab035.java
+++ Lab035.java
@@ -1,5 +1,6 @@
 public class MatrixMultiplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int[][] matrixA = {
             {1, 2},
             {3, 4}
