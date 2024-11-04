73d2351671df4cfc4787a1f2f3ce70ec943fdc9d
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-04 22:57:52 +0530

    Update Lab035.java

--- Lab035.java
+++ Lab035.java
@@ -1,17 +1,29 @@
-package July
-
-public class Lab035 {
+public class MatrixMultiplication {
     public static void main(String[] args) {
-        // Escape Chars
-        char c = '\n'; // New line / Next Line
-        char c1 = '\t'; // Add Tab
-        char c2 = '\b'; // Backlash, delete 1 char
-        char c3 = '\r'; // Backlash, delete 1 word
-//        System.out.println("Pramod" + c3 + "Dutta");
-//        System.out.println("Pramod" + c2 + "Dutta");
-//        System.out.println("Pramod" + c1 + "Dutta");
-        System.out.println("Pramod" + c + "Dutta");
+        int[][] matrixA = {
+            {1, 2},
+            {3, 4}
+        };
+        int[][] matrixB = {
+            {2, 0},
+            {1, 2}
+        };
+        int[][] product = new int[2][2];
 
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    product[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
 
+        System.out.println("Product of the matrices:");
+        for (int[] row : product) {
+            for (int elem : row) {
+                System.out.print(elem + " ");
+            }
+            System.out.println();
+        }
     }
 }
