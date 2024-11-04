c217e15cf2dd2c51eb5c3cb4a70265bd263f48ad
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-04 22:59:40 +0530

    Update MatrixMultiplication.java

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -17,6 +17,7 @@ public class MatrixMultiplication {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
                     product[i][j] += matrixA[i][k] * matrixB[k][j];
+
                 }
             }
         }
