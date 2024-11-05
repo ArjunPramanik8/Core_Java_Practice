007aa32556aa22e13a0447d7c19664068fdf170c
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-05 23:56:14 +0530

    Update lab242.java

--- lab242.java
+++ lab242.java
@@ -1,31 +1,13 @@
-package july.ex_20072024;
-
-import java.util.Scanner;
-
-public class Lab104 {
+public class AverageArray {
     public static void main(String[] args) {
-        //Program to Find the Largest Among Three Numbers:
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Num 1");
-        int num1 = sc.nextInt();
-        System.out.println("Enter the Num 2");
-        int num2 = sc.nextInt();
-        System.out.println("Enter the Num 3");
-        int num3 = sc.nextInt();
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
 
-
-        // num 1 > num2 && num1 > num3 ->  num1
-        // num 2 > num1 && num2 > num3 ->  num2
-        // num 3
-
-        if (num1 >= num2 && num1 >= num3) {
-            System.out.println(num1);
-        } else if (num2 >= num1 && num2 >= num3) {
-            System.out.println(num2);
-        } else {
-            System.out.println(num3);
+        for (int num : arr) {
+            sum += num;
         }
 
-
+        double average = (double) sum / arr.length;
+        System.out.println("Average: " + average);
     }
 }
