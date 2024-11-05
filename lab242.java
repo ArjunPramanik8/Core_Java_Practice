6424920029b2b7e970a88e902d185cb557d8aefc
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-05 23:56:37 +0530

    Update lab242.java

--- lab242.java
+++ lab242.java
@@ -1,6 +1,7 @@
 public class AverageArray 
     {
-    public static void main(String[] args) {
+    public static void main(String[] args)     
+    {
         int[] arr = {10, 20, 30, 40, 50};
         int sum = 0;
 
