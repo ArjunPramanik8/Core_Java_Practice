e00de5de3adbf43fce3a709a1090b896650eb714
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-05 23:55:32 +0530

    Update Lab036.java

--- Lab036.java
+++ Lab036.java
@@ -1,6 +1,7 @@
 public class OnlyDigits
      {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+     {
         String str = "12345";
         boolean isOnlyDigits = str.matches("[0-9]+");
 
