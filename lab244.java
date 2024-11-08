5e52533b34f68ca79d925acccb31dd6cf4d10f8b
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-08 17:03:06 +0530

    Update lab244.java

--- lab244.java
+++ lab244.java
@@ -1,31 +1,15 @@
-package july.ex_20072024;
-
-import java.util.Scanner;
-
-public class Lab104 {
+public class ArraySorted {
     public static void main(String[] args) {
-        //Program to Find the Largest Among Three Numbers:
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Num 1");
-        int num1 = sc.nextInt();
-        System.out.println("Enter the Num 2");
-        int num2 = sc.nextInt();
-        System.out.println("Enter the Num 3");
-        int num3 = sc.nextInt();
+        int[] arr = {10, 20, 30, 40, 50};
+        boolean isSorted = true;
 
-
-        // num 1 > num2 && num1 > num3 ->  num1
-        // num 2 > num1 && num2 > num3 ->  num2
-        // num 3
-
-        if (num1 >= num2 && num1 >= num3) {
-            System.out.println(num1);
-        } else if (num2 >= num1 && num2 >= num3) {
-            System.out.println(num2);
-        } else {
-            System.out.println(num3);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                isSorted = false;
+                break;
+            }
         }
 
-
+        System.out.println("Array is sorted: " + isSorted);
     }
 }
