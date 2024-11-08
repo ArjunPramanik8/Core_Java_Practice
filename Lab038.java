ddd0e53b4b1d505f86ef9d11d66fe4fd3985b437
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-08 17:02:37 +0530

    Update Lab038.java

--- Lab038.java
+++ Lab038.java
@@ -1,6 +1,7 @@
 public class MinMaxArray 
     {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
         int[] arr = {10, 20, 5, 40, 15};
         int min = arr[0];
         int max = arr[0];
