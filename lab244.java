d4f23007eb6e1f0337ed8b17e9d13c5269b97356
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-08 17:03:15 +0530

    Update lab244.java

--- lab244.java
+++ lab244.java
@@ -1,4 +1,5 @@
-public class ArraySorted {
+public class ArraySorted 
+    {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50};
         boolean isSorted = true;
