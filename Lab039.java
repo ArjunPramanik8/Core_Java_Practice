327fd41704623d142bbb702407d7a22bbd16bfe8
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-09 22:36:35 +0530

    Update Lab039.java

--- Lab039.java
+++ Lab039.java
@@ -1,6 +1,7 @@
 public class PerfectSquare 
     {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+            {
         int num = 16;
         double sqrt = Math.sqrt(num);
 
