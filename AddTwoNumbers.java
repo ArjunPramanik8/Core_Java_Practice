15cc9ef810b389cb89d15d80da884724757fb3a8
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-17 23:55:03 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,7 +1,8 @@
 public class AddTwoNumbers {
 
     public static void main(String[] args) 
-{       int a = 5, b = 10;
+{     
+    int a = 5, b = 10;
         int sum = a + b;
         System.out.println("Sum: " + sum);
  
