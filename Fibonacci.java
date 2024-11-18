9f0fc449acff8d2b0853a77d8e8e3a17ed9c0725
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-11-18 23:45:42 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,6 @@
 public class Fibonacci {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int n = 10, a = 0, b = 1;
         for (int i = 1; i <= n; ++i) {
             System.out.print(a + " ");
