7e361eee58943ced88463ae26fe171124044f470
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-15 22:51:21 +0530

    Update BubbleSort .java

--- BubbleSort .java
+++ BubbleSort .java
@@ -5,6 +5,7 @@ public class BubbleSort
             
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
         int n = arr.length;
+
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
