c187593846d466c5263fbdef5108c98e84771bb5
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-26 23:59:21 +0530

    Inheritance practice

---
+++ Triangle.java
@@ -0,0 +1,9 @@
+package OOPs_conecpt.Inheritance;
+
+public class Triangle extends Shape{
+
+    public void area(int l,int h)
+    {
+        System.out.println("Area of Triangle ="+ 0.5*l*h);
+    }
+}
