24ee2b2347396f170922513de3cbc20797b070f4
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-26 23:58:05 +0530

    Encapsulation practice

---
+++ Account.java
@@ -0,0 +1,24 @@
+package OOPs_conecpt.Encapsulation.Bank;
+
+public class Account {
+
+    public String name;
+    protected String email;
+    private String password;
+
+    public String getpassword() {
+        return this.password;
+    }
+
+    public void setpassword(String pass2) {
+        this.password = pass2;
+    }
+
+    public static void main(String[] args) {
+
+
+        Account a2= new Account();
+        a2.email="newjavacode@gmail.com";
+        a2.password="Abc@123";
+    }
+}
