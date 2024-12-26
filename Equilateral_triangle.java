049ef1c69a96c8d325c50be95a38fe7e56813948
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-26 23:58:49 +0530

    Inheritance practice

---
+++ Equilateral_triangle.java
@@ -0,0 +1,9 @@
+package OOPs_conecpt.Inheritance;
+
+public class Equilateral_triangle extends Triangle{
+
+    public void area(int side)
+    {
+        System.out.println("ARea of an equilateral triange is "+ side* side);
+    }
+}
