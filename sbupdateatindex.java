9a827dcd77cfe79938615242803bcaad2674004f
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-27 23:10:00 +0530

    Java Practice questions

---
+++ sbupdateatindex.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class sbupdateatindex {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb= new StringBuilder("I will make it happen");
+
+        System.out.println(sb.charAt(0));
+        sb.setCharAt(0,'U');
+
+        System.out.println(sb);
+
+    }
+}
