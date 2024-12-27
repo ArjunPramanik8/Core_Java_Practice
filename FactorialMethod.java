6b93a91bc532b82378c728f81b5b1f7eed757205
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-27 23:09:26 +0530

    Java Practice questions

---
+++ FactorialMethod.java
@@ -0,0 +1,27 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class FactorialMethod {
+
+    public static void printFactorial(int n)
+    {
+        int fact=1;
+
+        for (int i = 1; i <=n; i++) {
+
+            fact=fact*i;
+        }
+
+        System.out.println("Factorial "+fact);
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num= sc.nextInt();
+        printFactorial(num);
+    }
+}
