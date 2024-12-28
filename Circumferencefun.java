04de852cc81fb0010779a9545c329756d55eac24
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-28 23:58:45 +0530

    Java Fundamental Practice

---
+++ Circumferencefun.java
@@ -0,0 +1,23 @@
+package Practice;
+//Q.Write a function that takes in the radius as input and returns the circumference of a circle.
+
+import java.util.Scanner;
+
+public class Circumferencefun {
+
+    public static void circumference(Double r)
+    {
+        Double Circumference= 2*3.14*r;
+        System.out.println("Circumference of the circle is " + Circumference);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the radius");
+        Double radius= sc.nextDouble();
+
+        circumference(radius);
+    }
+
+}
