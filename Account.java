931c375dd907a7bf45b20faa28bba72fb1b90912
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2024-12-29 23:50:28 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -15,7 +15,6 @@ public class Account {
     
 
     public static void main(String[] args) {
-
         Account a2= new Account();
         a2.email="newjavacode@gmail.com";
         a2.password="Abc@123";
