2dba333e2f2fa84a7f58fa90e508502cad9b2094
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2025-01-05 23:51:28 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -2,7 +2,7 @@ package OOPs_conecpt.Encapsulation.Bank;
 
 public class Account {
     public String name;
-    protected String email;
+        protected String email;
         private String password;
 
     public String getpassword() {
