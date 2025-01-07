c2ca45e6e832f8bdb2c1efe4e53e859e6e9ef01d
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2025-01-07 23:45:45 +0530

    Update Equilateral_triangle.java'

--- Equilateral_triangle.java
+++ Equilateral_triangle.java
@@ -1,6 +1,7 @@
 package OOPs_conecpt.Inheritance;
 
-public class Equilateral_triangle extends Triangle{
+public class Equilateral_triangle extends Triangle
+{
     public void area(int side)
     {
         System.out.println("ARea of an equilateral triange is "+ side* side);
