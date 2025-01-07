dda161d9e51b3c065496ca5965c552fa14410fdf
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2025-01-07 23:47:17 +0530

    Update FactorialMethod.java

--- FactorialMethod.java
+++ FactorialMethod.java
@@ -14,7 +14,6 @@ public class FactorialMethod {
     }
 
     public static void main(String[] args) {
-
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the number");
         int num= sc.nextInt();
