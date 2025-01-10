c63af97fc265f04b0915dda7fe4c5cc697ba0a33
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2025-01-10 22:39:39 +0530

    Update Lab059.java

--- Lab059.java
+++ Lab059.java
@@ -2,6 +2,7 @@ package July
 
 public class Lab059 {
     public static void main(String[] args) {
+        
         int course = 100;
         float GST = 18.45F;
         float total_price = course+GST;
