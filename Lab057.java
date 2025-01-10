f2d8749f4ba544ef7eb4f3bf3d8804348cb9675b
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2025-01-10 22:38:33 +0530

    Update Lab057.java

--- Lab057.java
+++ Lab057.java
@@ -3,6 +3,7 @@ package July
 public class Lab057 {
     public static void main(String[] args) {
         long phone_no = 9876543210L;
+        
         // short s = phone_no; // Implicit Narrowing - JVM?
         short s = (short) phone_no;
         System.out.println(s);
