30513cf9cf8f94bf1901dffcc6028312d3a457b5
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2026-06-15 08:09:38 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -12,7 +12,6 @@ public class Account {
     public void setpassword(String pass2) {
         this.password = pass2;
     }
-
     
     public static void main(String[] args) {
         Account a2= new Account();
