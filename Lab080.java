4dbd8122caf20a9610533204945725b7ccdf2378
Arjun Pramanik <167803764+ArjunPramanik8@users.noreply.github.com>
2026-06-18 06:57:15 +0530

    Update Lab080.java

--- Lab080.java
+++ Lab080.java
@@ -2,7 +2,7 @@ package August
 
 public class Lab080 {
     public static void main(String[] args) {
-        // Max between two numbers
+        // Max between two numbers 
         int a= 10;
         int b= 20;
         int max = a > b ? a : b;
